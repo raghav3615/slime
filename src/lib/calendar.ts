c983a6c5de42a5ed1e98705247152b44aea8ab9c
raghav3615 <dadhichraghav896@gmail.com>
2025-11-12 22:24:45 +0530

    basic structure

--- src/lib/calendar.ts
+++
@@ -1,7 +0,0 @@
-import { google } from "googleapis"
-
-export function getCalendarClient(accessToken: string) {
-  const auth = new google.auth.OAuth2()
-  auth.setCredentials({ access_token: accessToken })
-  return google.calendar({ version: "v3", auth })
-}
